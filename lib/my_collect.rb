4a4c06a146bbf14395c048894779ead38cdde37b
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-27 01:36:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+list = ["Steve Carter", "Mike Jame", "John Snow"]
+def my_collection(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+  name_collection << yield(array[i])
+  i += 1
+  end
+  name_collection
+end
+my_collection(list) {|i| list.split(" ").reverse}
